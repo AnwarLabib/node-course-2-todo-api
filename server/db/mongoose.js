fe03622bdcd4ebc89d529a293ae6943426fc73ca
Anwar Labib <anwar.labib@student.guc.edu.eg>
2018-02-13 22:01:36 +0200

    TO DEPLOY

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGOHQ_URI||"mongodb://localhost:27017/TodoApp");
+mongoose.connect(process.env.MONGOHQ_URL||"mongodb://localhost:27017/TodoApp");
 
 module.exports = {mongoose};
