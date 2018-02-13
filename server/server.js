1987d2880e1e6e7ad29a2d05a5350f060702e97d
Anwar Labib <anwar.labib@student.guc.edu.eg>
2018-02-13 19:09:45 +0200

    Add GET routes

--- server/server.js
+++ server/server.js
@@ -21,6 +21,16 @@ app.post("/todos",(req,res)=>{
     });
 });
 
+app.get("/todos",(req,res)=>{
+    Todo.find().then((docs)=>{
+        res.send({docs});
+    },(err)=>{
+        res.status(400).send(e);
+    });
+});
+
 app.listen(3000,()=>{
     console.log("Started on port 3000");
 });
+
+module.exports = {app};
