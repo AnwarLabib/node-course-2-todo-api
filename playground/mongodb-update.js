79e8f2c8452fce38a637e3fd81c9a3b18ea8b7f6
Anwar Labib <anwar.labib@student.guc.edu.eg>
2018-02-13 15:47:18 +0200

    Adding the update file

---
+++ playground/mongodb-update.js
@@ -0,0 +1,41 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+// var obj = new ObjectID();
+// console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,client)=>{
+    if(err){
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    var db = client.db('TodoApp');
+
+    // db.collection('Todos').findOneAndUpdate({text:'Eat lunch'},
+    // {
+    //     $set : {
+    //         completed:true
+    //     }
+    // },{
+    //     returnOriginal:false
+    // }).then((result)=>{
+    //     console.log(result)
+    // });
+
+    db.collection('Users').findOneAndUpdate({name:'Anwar Labib'},
+    {
+        $set : {
+            name:'Rony'
+        },
+        $inc : {
+            age:1
+        }
+    },{
+        returnOriginal:false
+    }).then((result)=>{
+        console.log(result)
+    });
+
+
+//    client.close();
+});
